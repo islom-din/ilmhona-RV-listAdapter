1bcc9c54145da6d0b47aeccebef43680cb29f414
Khushbakht Boboev <form55x@bk.ru>
2023-01-21 15:15:20 +0300

    Khushbakht modif

--- app/src/main/java/islom/viewmodel/MyViewModel.kt
+++ app/src/main/java/islom/viewmodel/MyViewModel.kt
@@ -10,21 +10,21 @@ class MyViewModel {
 
     val newList = mutableListOf<Message>(
         Message(1,
-            Collage(R.drawable.ic_launcher_background,
-                R.drawable.ic_launcher_foreground,
-                R.drawable.ic_launcher_foreground),
+            Collage(R.drawable.c1g,
+                R.drawable.c1t,
+                R.drawable.c1b),
             null),
         Message(2,
             null,
-            SimpleMessage("This is background",R.drawable.ic_launcher_background)),
+            SimpleMessage("Garry Potter",R.drawable.c1g)),
         Message(3,
-            Collage(R.drawable.ic_launcher_foreground,
-                R.drawable.ic_launcher_background,
-                R.drawable.ic_launcher_background),
+            Collage(R.drawable.hobbit,
+                R.drawable.lord_of_the_rings,
+                R.drawable.game_of_thrones),
             null),
         Message(4,
             null,
-            SimpleMessage("This is Android`s ICON",R.drawable.ic_launcher_foreground))
+            SimpleMessage("Hobbit",R.drawable.hobbit))
     )
 
     fun getNewData(): MutableList<Message> = newList
