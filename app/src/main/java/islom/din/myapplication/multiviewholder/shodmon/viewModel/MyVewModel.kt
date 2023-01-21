7e1199d97e0dc4ebf310bbf0141487bcfad4893a
Shodmon <shodik.9393@gmail.com>
2023-01-21 17:46:43 +0500

    Finnal

--- app/src/main/java/islom/din/myapplication/multiviewholder/shodmon/viewModel/MyVewModel.kt
+++ app/src/main/java/islom/din/myapplication/multiviewholder/shodmon/viewModel/MyVewModel.kt
@@ -11,10 +11,13 @@ class MyVewModel {
         val items: MutableList<Message> = mutableListOf()
 
         items.add(Message(1, null, ImageMessage(R.drawable.game_of_thrones)))
-
         items.add(Message(2, SimpleMessage("Canada"), null))
-        items.add(Message(1, null, ImageMessage(R.drawable.game_of_thrones)))
-        items.add(Message(2, SimpleMessage("Tajikistan"), null))
+        items.add(Message(3, null, ImageMessage(R.drawable.game_of_thrones)))
+        items.add(Message(4, SimpleMessage("Tajikistan"), null))
+        items.add(Message(5, null, ImageMessage(R.drawable.pizza_001_copy)))
+        items.add(Message(6, SimpleMessage("Mother Russia"), null))
+        items.add(Message(5, null, ImageMessage(R.drawable.pizza_002)))
+
 
         return items
     }
